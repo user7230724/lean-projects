c60aada1f15d17af0066f3ddf96571b9f1ce1a65
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-03-19 18:37:25 +0100

    commit

---
+++ determinacy.lean
@@ -0,0 +1,47 @@
+import tactic.induction
+import data.int.basic
+import data.set.basic
+import logic.function.iterate
+
+import .game
+
+noncomputable theory
+open_locale classical
+
+lemma not_angel_hws_at_of {pw : ℕ} {s : State}
+  (h : devil_hws_at pw s) : ¬angel_hws_at pw s :=
+begin
+  cases h with d h, change ¬∃ _, _, push_neg,
+  intro a, use d, rw not_angel_wins_at, apply h,
+end
+
+lemma not_devil_hws_at_of {pw : ℕ} {s : State}
+  (h : angel_hws_at pw s) : ¬devil_hws_at pw s :=
+begin
+  cases h with d h, change ¬∃ _, _, push_neg,
+  intro a, use d, rw not_devil_wins_at, apply h,
+end
+
+-----
+
+lemma angel_hws_at_of {pw : ℕ} {s : State}
+  (h : ¬devil_hws_at pw s) : angel_hws_at pw s :=
+begin
+  sorry
+end
+
+-- #exit
+
+-----
+
+lemma devil_hws_at_of {pw : ℕ} {s : State}
+  (h : ¬angel_hws_at pw s) : devil_hws_at pw s :=
+by { contrapose! h, exact angel_hws_at_of h }
+
+lemma not_angel_hws_at_iff {pw : ℕ} {s : State} :
+  ¬angel_hws_at pw s ↔ devil_hws_at pw s :=
+⟨devil_hws_at_of, not_angel_hws_at_of⟩
+
+lemma not_devil_hws_at_iff {pw : ℕ} {s : State} :
+  ¬devil_hws_at pw s ↔ angel_hws_at pw s :=
+⟨angel_hws_at_of, not_devil_hws_at_of⟩
--- main.lean
+++ main.lean
@@ -4,7 +4,7 @@ import data.set.basic
 import logic.function.iterate
 
 import .point .dist .board .state .move .game
-import .angel_pw_ge
+import .determinacy .angel_pw_ge
 
 noncomputable theory
 open_locale classical
@@ -58,7 +58,7 @@ end
 
 -----
 
-theorem angel_hws_iff_pw_ge_2 {pw : ℕ} :
+lemma angel_hws_iff_pw_ge_2 {pw : ℕ} :
   angel_hws pw ↔ 2 ≤ pw :=
 begin
   cases pw, simp [angel_pw_0_not_hws],
@@ -66,7 +66,7 @@ begin
   refine angel_pw_ge_hws _ angel_pw_2_hws, simp [nat.succ_le_succ],
 end
 
-theorem exi_pw_angel_hws : ∃ (pw : ℕ), angel_hws pw :=
+lemma exi_pw_angel_hws : ∃ (pw : ℕ), angel_hws pw :=
 ⟨2, angel_pw_2_hws⟩
 
 -- example {a : ℤ}
