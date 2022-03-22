341df5ec4f698d8300fbd1d8e4275137ccd6b48b
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-03-22 03:06:09 +0100

    commit

--- determinacy.lean
+++ determinacy.lean
@@ -24,16 +24,6 @@ end
 
 -----
 
--- lemma act_of_not_devil_hvs {pw : ℕ} {g : Game pw}
---   (h : ¬devil_hws_at pw g.s) : g.act :=
--- begin
---   change ¬∃ _, _ at h, push_neg at h,
---   specialize h g.d,
---   cases h with a₁ h,
--- end
-
--- #exit
-
 lemma exi_angel_move_of_not_devil_hws {pw : ℕ} {s : State}
   (h : ¬devil_hws_at pw s) :
   ∀ (md : Valid_devil_move s.board),
@@ -57,69 +47,7 @@ end
 lemma angel_hws_at_of {pw : ℕ} {s : State}
   (h : ¬devil_hws_at pw s) : angel_hws_at pw s :=
 begin
-  use mk_angel_st_for_not_devil_hws pw,
-  rintro d n,
-  let g : Game pw := _, change (g.play n).act,
-  suffices h₁ : ¬devil_hws_at pw (g.play n).s ∧ _, exact h₁.2,
-  induction n with n ih,
-  { exact ⟨h, trivial⟩ },
-  {
-    rw play_at_succ',
-    let g₁ : Game pw := _, change g.play n with g₁ at ih ⊢,
-    cases ih with ih₁ ih₂,
-    have hh := exi_angel_move_of_not_devil_hws ih₁,
-    fsplit,
-    {
-      have ih' := ih₁,
-      change ¬∃ _, _ at ih', push_neg at ih',
-      change ¬∃ _, _, push_neg,
-      intro d',
-      specialize ih' d',
-      cases ih' with a' ih',
-      simp_rw not_devil_wins_at at ih' ⊢,
-      use a',
-      intro k,
-      specialize ih' k.succ,
-      rw play_at_succ at ih',
-      convert ih',
-      symmetry,
-      have h₁ : (⟨a', d, g₁.s, true⟩ : Game _) = g₁,
-      {
-        ext,
-        {
-          sorry
-        },
-        sorry,
-        sorry,
-        sorry,
-      },
-      sorry
-      -- have h₂ : g₁.play_move = g₁.set_state g₁.play_move.s,
-      -- {
-      --   clear h₁,
-      --   apply play_move_eq_set_state_of_act_next,
-      --   rw play_move_at_act ih₂,
-      --   let g₁' : Game pw := _, change play_devil_move_at g₁ with g₁',
-      --   change Game.act (dite _ _ _),
-      --   split_ifs with h₁,
-      --   {
-      --     exact ih₂,
-      --   },
-      --   {
-      --     specialize hh (d.f g₁.s),
-      --     cases hh with ma hh,
-      --     apply h₁,
-      --     use ma.m,
-      --     convert ma.h,
-      --     change apply_devil_move _ _ = _,
-      --     congr,
-      --     apply play_at_players_eq.2,
-      --   },
-      -- },
-      -- convert h₂; rw ←h₁,
-    },
-    sorry,
-  },
+  sorry
 end
 
 #exit
