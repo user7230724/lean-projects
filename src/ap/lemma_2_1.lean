cd9c047cf5022d033bf1bd7efbab0897b8752415
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-04-08 14:30:40 +0200

    commit

--- src/ap/lemma_2_1.lean
+++ src/ap/lemma_2_1.lean
@@ -17,39 +17,25 @@ def Bounded (r : ℕ) : set Point :=
 def trapped_in {pw : ℕ} (a : A pw) (d : D) (B : set Point) :=
 all_b a d (λ b, b.A ∈ B)
 
--- This is wrong
--- Change `s` with `s'`
-lemma exi_ma_inf_n_act_of_exi_wins {pw : ℕ} {d : D} {s : State}
-  (hs : s.act) (hvm : A_has_valid_move pw s.board)
+-- lemma exi_ma_inf_n_act_of_exi_wins {pw : ℕ} {d : D} {s s' : State} {hs}
+--   (h₁ : ∀ (n : ℕ), ∃ (a : A pw), ((init_game a d s).play n).act)
+--   (h₂ : s' = apply_D_move s (d.f s hs).m) :
+--   ∃ (ma : Valid_A_move pw s'.board), {n : ℕ | ∃ (a : A pw) hs' hvm,
+--   a.f s' hs' hvm = ma ∧ ((init_game a d s).play n).act}.infinite :=
+-- begin
+--   sorry
+-- end
+
+-- def mk_A_for_lem_2_1 (pw : ℕ) (d : D) (s : State) : A pw := ⟨λ s hs hvm,
+-- if h : ∀ (n : ℕ), ∃ (a : A pw), ((init_game a d s).play n).act
+-- then (exi_ma_inf_n_act_of_exi_wins h₁ h₂).some
+-- else ⟨_, hvm.some_spec⟩⟩
+
+lemma exi_A_forall_n_play_act_of_swap {pw : ℕ} {s : State} {d : D}
   (h : ∀ (n : ℕ), ∃ (a : A pw), ((init_game a d s).play n).act) :
-  ∃ (ma : Valid_A_move pw s.board), {n : ℕ | ∃ (a : A pw),
-  a.f s hs hvm = ma ∧ ((init_game a d s).play n).act}.infinite :=
+  ∃ (a : A pw), ∀ (n : ℕ), ((init_game a d s).play n).act :=
 begin
   sorry
-end
-
-def mk_A_for_lem_2_1 (pw : ℕ) (d : D) : A pw := ⟨λ s hs hvm,
-if h : ∀ (n : ℕ), ∃ (a : A pw), ((init_game a d s).play n).act
-then (exi_ma_inf_n_act_of_exi_wins hs hvm h).some
-else ⟨_, hvm.some_spec⟩⟩
-
-lemma mk_A_for_lem_2_1_wins_at_play {pw n : ℕ} {s : State} {d : D}
-  (h : ∀ (n : ℕ), ∃ (a : A pw), ((init_game a d s).play n).act) :
-  ((init_game (mk_A_for_lem_2_1 pw d) d s).play n).act :=
-begin
-  have hs : s.act,
-  { specialize h 0, cases h with a h, exact h },
-  induction n with n ih,
-  { exact hs },
-  {
-    rw play_at_succ',
-    let a : A pw := _,
-    change mk_A_for_lem_2_1 pw d with a at ih,
-    let g : Game pw := _,
-    change g.act at ih,
-    change g.play_move.act,
-    sorry
-  },
 end
 
 #exit
@@ -61,12 +47,14 @@ lemma D_wins_n_of_D_hws {pw : ℕ}
 begin
   contrapose! h, rw forall_swap at h, change ∀ (d : D) (n : ℕ), _ at h,
   simp_rw [D_wins_in, simulate] at h, push_neg at h, rw [D_hws, D_hws_at],
-  push_neg, intro d, specialize h d, let a := mk_A_for_lem_2_1 pw d, use a,
+  push_neg, intro d, specialize h d,
   replace h : ∀ (n : ℕ), ∃ (a : A pw), ((init_game a d state₀).play n).act,
   { intro n, specialize h n, cases h with a h, use a, rcases h with ⟨k, h₁, h₂⟩,
     obtain ⟨k, rfl⟩ := nat.exists_eq_add_of_le h₁, apply act_play_at_le h₁ h₂ },
-  rw Game.D_wins, push_neg, intro n, exact mk_A_for_lem_2_1_wins_at_play h,
+  simp_rw Game.D_wins, push_neg, exact exi_A_forall_n_play_act_of_swap h,
 end
+
+#exit
 
 lemma A_bounded_n_pw {pw n k : ℕ} {a : A pw} {d : D}
   (h : k ≤ n) :
