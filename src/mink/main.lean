de586f0a099cd07127ba3a25ff7e4e201b2304fe
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-04-13 05:49:26 +0200

    commit

--- src/mink/main.lean
+++ src/mink/main.lean
@@ -5,26 +5,21 @@ noncomputable theory
 open_locale classical
 
 inductive Expr
-| Bot : Expr
 | K : Expr
 | S : Expr
 | M : Expr
 | Call : Expr → Expr → Expr
 open Expr
 
-instance : has_bot Expr := ⟨Bot⟩
-
-infixl ~:100 := Call
+infixl ` ~ `:100 := Call
 
 inductive Reduces : Expr → Expr → Prop
-| bot (a) : Reduces (⊥ ~ a) ⊥
 | k (a b) : Reduces (K ~ a ~ b) a
 | s (a b c) : Reduces (S ~ a ~ b ~ c) (a ~ c ~ (b ~ c))
-| mb : Reduces (M ~ ⊥) ⊥
 | mk : Reduces (M ~ K) K
 | ms : Reduces (M ~ S) S
-| mm : Reduces (M ~ M) ⊥
-| call {a b c} (h₁ : Reduces a b) : Reduces (a ~ c) (b ~ c)
+| left {a b c} : Reduces a b → Reduces (a ~ c) (b ~ c)
+| right {a b c} : Reduces a b → Reduces (c ~ a) (c ~ b)
 
 inductive isK : Expr → Prop
 | triv : isK K
@@ -33,6 +28,16 @@ inductive isK : Expr → Prop
 -----
 
 def I := S ~ K ~ K
+
+inductive hasK : Expr → Prop
+| triv : hasK K
+| left (a b) : hasK a → hasK (a ~ b)
+| right (a b) : hasK b → hasK (a ~ b)
+
+lemma hasK_of_isK {a : Expr} (h : isK a) : hasK a :=
+begin
+  sorry
+end
 
 example : ¬isK (S ~ I ~ I ~ (S ~ I ~ I)) :=
 begin
