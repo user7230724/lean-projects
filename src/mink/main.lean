d8dce30217dd5937a64d4bcd37bfb8869a587077
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-04-17 17:40:15 +0200

    commit

--- src/mink/main.lean
+++ src/mink/main.lean
@@ -14,13 +14,14 @@ open Expr
 infixl ` ~ `:100 := App
 
 inductive Reduces : Expr → Expr → Prop
-| k (a b) : Reduces (K ~ a ~ b) a
-| s (a b c) : Reduces (S ~ a ~ b ~ c) (a ~ c ~ (b ~ c))
+| refl {a} : Reduces a a
+| trans {a b c} : Reduces a b → Reduces b c → Reduces a c
+| k {a b} : Reduces (K ~ a ~ b) a
+| s {a b c} : Reduces (S ~ a ~ b ~ c) (a ~ c ~ (b ~ c))
 | mk : Reduces (M ~ K) K
 | ms : Reduces (M ~ S) S
 | left {a b c} : Reduces a b → Reduces (a ~ c) (b ~ c)
 | right {a b c} : Reduces a b → Reduces (c ~ a) (c ~ b)
-| trans {a b c} : Reduces a b → Reduces b c → Reduces a c
 
 infix ` ==> `:50 := Reduces
 
@@ -28,21 +29,48 @@ infix ` ==> `:50 := Reduces
 
 def I := S ~ K ~ K
 
+@[refl]
+lemma reduces_refl {a : Expr} : a ==> a :=
+Reduces.refl
+
+@[trans]
+lemma reduces_trans {a b c : Expr} (h₁ : a ==> b) (h₂ : b ==> c) : a ==> c :=
+h₁.trans h₂
+
+lemma K_reduces_to_iff_eq_K {a : Expr} : K ==> a ↔ a = K :=
+begin
+  split; intro h,
+  { induction' h,
+    { refl },
+    { subst b, apply ih_h, refl }},
+  { subst h },
+end
+
+lemma not_K_reduces_S : ¬K ==> S :=
+by simp [K_reduces_to_iff_eq_K]
+
+lemma I_app_reduces_to_self {a : Expr} : I ~ a ==> a :=
+begin
+  transitivity, apply Reduces.s,
+  transitivity, apply Reduces.k,
+  refl,
+end
+
 -----
 
-meta def reduce' : Expr → option Expr
-| (K ~ a ~ b) := some a
-| (S ~ a ~ b ~ c) := some (a ~ c ~ (b ~ c))
-| (M ~ K) := some K
-| (M ~ S) := some S
-| _ := none
+-- meta def reduce' : Expr → option Expr
+-- | (K ~ a ~ b) := some a
+-- | (S ~ a ~ b ~ c) := some (a ~ c ~ (b ~ c))
+-- | (M ~ K) := some K
+-- | (M ~ S) := some S
+-- | _ := none
 
-meta def reduce : Expr → Expr
-| e@(a ~ b) := match reduce' e with
-  | some e₁ := reduce e₁
-  | none := match reduce' a with
-    | some a₁ := reduce (a₁ ~ b)
-    | none := e
-  end
-end
-| a := a
+-- meta def reduce : Expr → Expr
+-- | e@(a ~ b) := match reduce' e with
+--   | some e₁ := reduce e₁
+--   | none := match reduce' a with
+--     | some a₁ := reduce (a₁ ~ b)
+--     | none := e
+--   end
+-- end
+-- | a := a
