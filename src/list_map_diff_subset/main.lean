88e3b6140e056201a240b159e5aeb9f9e9fce9b8
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-04-26 16:37:07 +0200

    commit

--- src/list_map_diff_subset/main.lean
+++ src/list_map_diff_subset/main.lean
@@ -7,13 +7,6 @@ noncomputable theory
 open_locale classical
 open_locale big_operators
 
--- lemma list.count_cons_cons {α : Type*} {l : list α} {hd₁ hd₂ x : α} :
---   (hd₁ :: hd₂ :: l).count x = (hd₂ :: hd₁ :: l).count x :=
--- begin
---   simp_rw list.count_cons,
---   split_ifs; refl,
--- end
-
 lemma nat.pred_eq_self_iff {n : ℕ} : n.pred = n ↔ n = 0 :=
 begin
   cases n; simp,
@@ -29,8 +22,6 @@ begin
     { contrapose! h, exact list.mem_of_mem_erase h },
     rw [list.count_eq_zero_of_not_mem h, list.count_eq_zero_of_not_mem h₁] },
 end
-
--- #exit
 
 lemma list.mem_diff_iff {α : Type*} {l₁ l₂ : list α} {x : α} :
   x ∈ l₁ \ l₂ ↔ l₂.count x < l₁.count x :=
@@ -52,73 +43,80 @@ begin
   { rw list.count_erase_of_ne h },
 end
 
--- lemma list.count_map_le_count_map {α : Type}
---   {P : α → Prop} {l₁ l₂ : list α}
---   (h : ∀ (a : α), P a → l₁.count a ≤ l₂.count a) :
---   l₁.count 
-
--- #exit
-
-lemma list.length_eq_sum_count {α : Type*}
-  {l : list α} : l.length = ∑ (x : α) in l.to_finset, l.count x :=
+lemma finset.sum_le_sum_of_le {α : Type*} {f g : α → ℕ} {s : finset α}
+  (h : ∀ (a : α), a ∈ s → f a ≤ g a) :
+  ∑ (a : α) in s, f a ≤ ∑ (a : α) in s, g a :=
 begin
-  sorry
-  -- induction l with hd l ih; simp,
-  -- rw ih, clear ih,
-  -- let s := l.to_finset,
-  -- let s' := insert hd s,
-  -- change l.to_finset with s,
-  -- change insert hd s with s',
-  -- have h : hd ∈ s',
-  -- sorry,
-  -- rw finset.sum_eq_sum_diff_singleton_add h,
-  -- have h₁ : ∑ (x : α) in s' \ {hd}, list.count x (hd :: l) =
-  --   ∑ (x : α) in s' \ {hd}, list.count x l,
-  -- sorry,
-  -- rw h₁, clear h₁,
+  revert h, apply s.induction_on; simp,
+  rintro a s h₁ h₂ h₃ h₄, specialize h₂ h₄,
+  simp_rw finset.sum_insert h₁, exact add_le_add h₃ h₂,
 end
 
--- #exit
+lemma list.sum_count_cons_diff_singleton {α : Type*} {l : list α} {x : α} :
+  ∑ (a : α) in l.to_finset \ {x}, (x :: l).count a =
+  ∑ (a : α) in l.to_finset \ {x}, l.count a :=
+begin
+  apply finset.sum_congr rfl, rintro a h, rw [list.count_cons, if_neg],
+  rintro rfl, rw [finset.mem_sdiff, finset.mem_singleton] at h, exact h.2 rfl,
+end
+
+lemma list.sum_count_cons {α : Type*} {l : list α} {x : α} :
+  ∑ (a : α) in l.to_finset, (x :: l).count a =
+  ∑ (a : α) in l.to_finset, l.count a + ite (x ∈ l) 1 0 :=
+begin
+  simp_rw ←list.mem_to_finset, split_ifs,
+  { simp_rw finset.sum_eq_sum_diff_singleton_add h,
+    rw [list.sum_count_cons_diff_singleton, add_assoc],
+    congr, rw [list.count_cons, if_pos rfl] },
+  { have h₁ : l.to_finset = l.to_finset \ {x},
+    { exact (finset.sdiff_singleton_not_mem_eq_self _ h).symm },
+    rw h₁, exact list.sum_count_cons_diff_singleton },
+end
+
+lemma list.length_eq_sum_count {α : Type*} {l : list α} :
+  l.length = ∑ (x : α) in l.to_finset, l.count x :=
+begin
+  induction l with hd l ih; simp, by_cases h : hd ∈ l.to_finset,
+  { rw finset.insert_eq_of_mem h,
+    rw finset.sum_eq_sum_diff_singleton_add h at ih ⊢,
+    rw [list.sum_count_cons_diff_singleton, list.count_cons, if_pos rfl, ih],
+    refl },
+  { have h₁ : hd ∈ insert hd l.to_finset := finset.mem_insert_self _ _,
+    rw finset.sum_eq_sum_diff_singleton_add h₁, have h₂ : hd ∉ l,
+    { rwa list.mem_to_finset at h },
+    have h₃ : insert hd l.to_finset \ {hd} = l.to_finset,
+    { ext x, by_cases h₃ : x = hd,
+      { subst h₃, simpa [h₁] },
+      { simp [h₃] }},
+    rw h₃, have h₃ : list.count hd (hd :: l) = 1,
+    { rw [list.count_cons, if_pos rfl, list.count_eq_zero_of_not_mem h₂] },
+    rw h₃, congr, rw [ih, list.sum_count_cons, if_neg h₂], refl },
+end
+
+lemma list.sum_union_eq_sum {α : Type*} {l : list α} {s : finset α} :
+  ∑ (x : α) in l.to_finset ∪ s, l.count x = ∑ (x : α) in l.to_finset, l.count x :=
+begin
+  apply s.induction_on; simp, rintro a s h₁ ih,
+  by_cases h₃ : a ∈ l.to_finset ∪ s,
+  { rwa finset.insert_eq_of_mem h₃ },
+  { rw [finset.sum_insert h₃, ih], have h₄ : a ∉ l,
+    { contrapose! h₃, apply finset.mem_union_left, rwa list.mem_to_finset },
+    rw [list.count_eq_zero_of_not_mem h₄, zero_add] },
+end
 
 lemma list.length_le_length_of_count_le_count {α : Type*}
   {l₁ l₂ : list α} (h : ∀ (a : α), l₁.count a ≤ l₂.count a) :
   l₁.length ≤ l₂.length :=
 begin
-  sorry
-  -- induction l₁ with x l₁ ih generalizing l₂; simp,
-  -- cases l₂ with y l₂,
-  -- {
-  --   specialize h x,
-  --   rw list.count_cons at h,
-  --   rw if_pos rfl at h,
-  --   cases h,
-  -- },
-  -- {
-  --   simp,
-  --   apply ih, clear ih,
-  --   intro a,
-  --   have h₁ := h a,
-  --   simp_rw list.count_cons at h₁,
-  --   split_ifs at h₁ with h₂ h₃ h₄,
-  --   {
-  --     rwa nat.succ_le_succ_iff at h₁,
-  --   },
-  --   {
-  --     exact nat.le_of_succ_le h₁,
-  --   },
-  --   {
-  --     subst a,
-  --     replace h₂ : x ≠ y := by tauto,
-  --     replace h := id h,
-  --     replace h₁ := id h₁,
-  --   },
-  --   {
-  --     exact h₁,
-  --   },
-  -- },
+  simp_rw list.length_eq_sum_count,
+  let s := l₁.to_finset ∪ l₂.to_finset,
+  suffices h₁ : ∑ (x : α) in s, l₁.count x ≤ ∑ (x : α) in s, l₂.count x,
+  { change s with _ ∪ _ at h₁, nth_rewrite 1 finset.union_comm at h₁,
+    simp_rw list.sum_union_eq_sum at h₁, exact h₁ },
+  replace h : ∀ (a : α), a ∈ s → l₁.count a ≤ l₂.count a,
+  { rintro a h₁, apply h },
+  exact finset.sum_le_sum_of_le h,
 end
-
-#exit
 
 lemma list.count_map_eq_length_filter {α β : Type*} {f : α → β}
   {l : list α} {y : β} :
@@ -131,52 +129,31 @@ begin
   { rw if_neg, tauto },
 end
 
+lemma list.count_filter_eq_ite {α : Type*} {P : α → Prop}
+  {l : list α} {x : α} : (l.filter P).count x = ite (P x) (l.count x) 0 :=
+begin
+  split_ifs,
+  { exact list.count_filter h },
+  { apply list.count_eq_zero_of_not_mem,
+    contrapose! h, exact list.of_mem_filter h },
+end
+
 example {α β : Type*} {f : α → β} {l₁ l₂ : list α} :
   l₁.map f \ l₂.map f ⊆ (l₁ \ l₂).map f :=
 begin
-  rintro y h,
-  rw list.mem_diff_iff at h,
-  rw list.mem_map,
-  contrapose! h,
-  simp_rw [list.mem_diff_iff, imp_not_comm, not_lt] at h,
-
-  let s₁ := l₁.filter (λ x, f x = y),
-  let s₂ := l₂.filter (λ x, f x = y),
-
+  rintro y h, rw list.mem_diff_iff at h, rw list.mem_map,
+  contrapose! h, simp_rw [list.mem_diff_iff, imp_not_comm, not_lt] at h,
+  let s₁ := l₁.filter (λ x, f x = y), let s₂ := l₂.filter (λ x, f x = y),
   have hs₁ : ∀ (a : α), s₁.count a = ite (f a = y) (l₁.count a) 0,
-  sorry;{
-    rintro a,
-    change s₁ with list.filter _ _,
-    split_ifs with h₁,
-    {
-      rw list.count_filter,
-      exact h₁,
-    },
-    {
-      apply list.count_eq_zero_of_not_mem,
-      rw list.mem_filter,
-      simp [h₁],
-    },
-  },
+  { exact λ _, list.count_filter_eq_ite },
   have hs₂ : ∀ (a : α), s₂.count a = ite (f a = y) (l₂.count a) 0,
-  sorry,
-
+  { exact λ _, list.count_filter_eq_ite },
   replace h : ∀ (a : α), s₁.count a ≤ s₂.count a,
-  sorry;{
-    rintro a,
-    specialize h a,
-    rw [hs₁, hs₂],
+  { rintro a, specialize h a, rw [hs₁, hs₂],
     split_ifs with h₁,
-    {
-      exact h h₁,
-    },
-    {
-      refl
-    },
-  },
-
+    { exact h h₁ },
+    { refl }},
   have hh₁ : (l₁.map f).count y = s₁.length := list.count_map_eq_length_filter,
   have hh₂ : (l₂.map f).count y = s₂.length := list.count_map_eq_length_filter,
-  rw [hh₁, hh₂],
-  exact list.length_le_length_of_count_le_count h,
+  rw [hh₁, hh₂], exact list.length_le_length_of_count_le_count h,
 end
