366b31b7e00f1b633f96716d2bafd1ab3723ce7b
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-04-28 22:46:28 +0200

    commit

--- src/mink/main.lean
+++ src/mink/main.lean
@@ -20,6 +20,7 @@ def reduce : Expr → Expr
 | (M ~ K) := K
 | (M ~ S) := S
 | (M ~ a) := M ~ reduce a
+| (a ~ b) := reduce a ~ b
 | a := a
 
 def R (e₁ e₂ : Expr) : Prop :=
@@ -34,25 +35,50 @@ lemma R.refl {a} : a ==> a :=
 by use [0, rfl]
 
 @[trans]
-lemma R.trans' {a b c} (h₁ : a ==> b) (h₂ : b ==> c) : a ==> c :=
+lemma R.trans {a b c} (h₁ : a ==> b) (h₂ : b ==> c) : a ==> c :=
 begin
   cases h₁ with n₁ h₁, cases h₂ with n₂ h₂, use n₂ + n₁,
   rw function.iterate_add_apply, substs h₁ h₂,
 end
 
-lemma not_R_iff {a b} : ¬a ==> b ↔ ∀ n, b ≠ (reduce^[n]) a :=
+lemma not_R {a b} : ¬a ==> b ↔ ∀ (n : ℕ), b ≠ (reduce^[n]) a :=
 by simp [R]
 
 -----
+
+def term : Expr → Prop
+| (_ ~ _) := false
+| _ := true
+
+def app (a : Expr) : Prop :=
+¬term a
+
+lemma term.induct {P : Expr → Prop} {a : Expr}
+  (h₁ : term a) (h₂ : P K) (h₃ : P S) (h₄ : P M) : P a :=
+by cases a; simp [*, term] at *
 
 lemma R_iff_of_reduce_eq_self_iff {a b}
   (h : reduce a = a) : a ==> b ↔ a = b :=
 begin
   split; intro h₁,
-  { contrapose h₁, rw [not_R_iff], intro n, induction n with n ih,
+  { contrapose h₁, rw not_R, intro n, induction n with n ih,
     { tauto },
     { simp [h, ih] } },
   { subst h₁ },
+end
+
+lemma reduce_self_of_term {a} (h : term a) : reduce a = a :=
+by apply term.induct h; refl
+
+lemma R_iff_of_term {a b}
+  (h : term a) : a ==> b ↔ a = b :=
+R_iff_of_reduce_eq_self_iff (reduce_self_of_term h)
+
+lemma reduce_arg_app_R_term {t a b b'}
+  (h₁ : term t) (h₂ : b ==> b') :
+  a ~ b ==> t ↔ a ~ b' ==> t :=
+begin
+  sorry
 end
 
 -----
