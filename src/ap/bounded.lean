847c0d836b80c9050a1cef057095e3000e7cf6a6
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-05-01 14:40:53 +0200

    commit

--- src/ap/bounded.lean
+++ src/ap/bounded.lean
@@ -35,15 +35,19 @@ lemma mem_bounded_ge {n k : ℕ} {p : Point}
 by { simp [bounded] at h₂ ⊢, exact le_trans h₁ h₂ }
 
 lemma bounded_area_eq {n : ℕ} :
-  bounded_area = (n * 2 + 1) ^ 2 :=
+  bounded_area n = (n * 2 + 1) ^ 2 :=
 begin
   sorry
 end
 
+#exit
+
 lemma squares_in_bounded_le_area {n : ℕ} {b : Board} :
   squares_in_bounded b n ≤ bounded_area n :=
 begin
-  sorry
+  apply finset.card_le_card_of_inj_on id; rintro p hp,
+  { rw finset.mem_filter at hp, tauto },
+  { tauto },
 end
 
 lemma squares_in_bounded_exc_A_le_area {n : ℕ} {b : Board} :
