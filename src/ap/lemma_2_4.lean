c1631f5fc6592f3d24921de421a04346017e5784
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-05-01 11:41:47 +0200

    commit

--- src/ap/lemma_2_4.lean
+++ src/ap/lemma_2_4.lean
@@ -1,14 +1,14 @@
 import tactic
 import tactic.induction
 
-import .base .nice
+import .base .bounded .nice
 
 noncomputable theory
 open_locale classical
 
 lemma lem_2_4 {pw N : ℕ}
-  (h : ∃ (d : D), ∀ (a : A pw), A_trapped_in_for a d (Bounded N)) :
-  ∃ (d : D), d.nice pw ∧ ∀ (a : A pw), A_trapped_in_for a d (Bounded N) :=
+  (h : ∃ (d : D), ∀ (a : A pw), A_trapped_in_for a d (bounded N)) :
+  ∃ (d : D), d.nice pw ∧ ∀ (a : A pw), A_trapped_in_for a d (bounded N) :=
 begin
   sorry
 end
