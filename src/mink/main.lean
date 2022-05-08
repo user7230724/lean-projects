62d055ad1dfc8a5d02e00c439fbfbee288d1e941
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-05-08 10:06:45 +0200

    commit

--- src/mink/main.lean
+++ src/mink/main.lean
@@ -14,35 +14,35 @@ open Expr
 
 infixl ` ~ `:100 := App
 
-def reduce : Expr → Expr
+def step : Expr → Expr
 | (K ~ a ~ b) := a
 | (S ~ a ~ b ~ c) := a ~ c ~ (b ~ c)
 | (M ~ K) := K
 | (M ~ S) := S
-| (M ~ a) := M ~ reduce a
-| (a ~ b) := reduce a ~ b
+| (M ~ a) := M ~ step a
+| (a ~ b) := step a ~ b
 | a := a
 
-def R (e₁ e₂ : Expr) : Prop :=
-∃ (n : ℕ), (reduce^[n]) e₁ = e₂
+def reduces (e₁ e₂ : Expr) : Prop :=
+∃ (n : ℕ), (step^[n]) e₁ = e₂
 
-infix ` ==> `:50 := R
+infix ` ⇝ `:50 := reduces
 
 -----
 
 @[refl]
-lemma R.refl {a} : a ==> a :=
+lemma reduces.refl {a} : a ⇝ a :=
 by use [0, rfl]
 
 @[trans]
-lemma R.trans {a b c} (h₁ : a ==> b) (h₂ : b ==> c) : a ==> c :=
+lemma reduces.trans {a b c} (h₁ : a ⇝ b) (h₂ : b ⇝ c) : a ⇝ c :=
 begin
   cases h₁ with n₁ h₁, cases h₂ with n₂ h₂, use n₂ + n₁,
   rw function.iterate_add_apply, substs h₁ h₂,
 end
 
-lemma not_R {a b} : ¬a ==> b ↔ ∀ (n : ℕ), (reduce^[n]) a ≠ b :=
-by simp [R]
+lemma not_reduces {a b} : ¬a ⇝ b ↔ ∀ (n : ℕ), (step^[n]) a ≠ b :=
+by simp [reduces]
 
 -----
 
@@ -57,33 +57,56 @@ lemma term.induct {P : Expr → Prop} {a : Expr}
   (h₁ : term a) (h₂ : P K) (h₃ : P S) (h₄ : P M) : P a :=
 by cases a; simp [*, term] at *
 
-lemma R_iff_eq_of_reduce_eq_self {a b}
-  (h : reduce a = a) : a ==> b ↔ a = b :=
+lemma reduces_iff_eq_of_step_eq_self {a b}
+  (h : step a = a) : a ⇝ b ↔ a = b :=
 begin
   split; intro h₁,
-  { contrapose h₁, rw not_R, intro n, induction n with n ih,
+  { contrapose h₁, rw not_reduces, intro n, induction n with n ih,
     { tauto },
     { simp [h, ih] } },
   { subst h₁ },
 end
 
-lemma reduce_self_of_term {a} (h : term a) : reduce a = a :=
+lemma step_self_of_term {a} (h : term a) : step a = a :=
 by apply term.induct h; refl
 
-lemma R_iff_of_term {a b}
-  (h : term a) : a ==> b ↔ a = b :=
-R_iff_eq_of_reduce_eq_self (reduce_self_of_term h)
+lemma reduces_iff_of_term {a b}
+  (h : term a) : a ⇝ b ↔ a = b :=
+reduces_iff_eq_of_step_eq_self (step_self_of_term h)
 
-lemma reduce_arg_app_R_term {t f a a'}
-  (h₁ : term t) (h₂ : a ==> a') :
-  f ~ a ==> t ↔ f ~ a' ==> t :=
+-----
+
+lemma apps_reduce_iff_args_reduce {t f : Expr} {args : list (Expr × Expr)}
+  (h₁ : term t)
+  (h₂ : ∀ (p : Expr × Expr), p ∈ args → p.1 ⇝ p.2) :
+  function.uncurry (λ a b, a ⇝ t ↔ b ⇝ t)
+    (list.foldl (λ (a p : Expr × Expr), (a.1 ~ p.1, a.2 ~ p.2)) ⟨f, f⟩ args) :=
 begin
-  sorry
+  let m : Expr × Expr := _,
+  change function.uncurry _ m,
+  change _ ↔ _,
+  simp_rw reduces,
+  split; intro h,
+  {
+    sorry
+  },
+  {
+    sorry
+  },
+end
+
+lemma app_reduces_iff_arg_reduces {t f a b}
+  (h₁ : term t)
+  (h₂ : a ⇝ b) :
+  f ~ a ⇝ t ↔ f ~ b ⇝ t :=
+begin
+  apply @apps_reduce_iff_args_reduce t f [(a, b)] h₁ _, rintro p h₄,
+  rw list.mem_singleton at h₄, subst p, exact h₂,
 end
 
 -----
 
 def I := S ~ K ~ K
 
-lemma I_id {a} : I ~ a ==> a :=
+lemma I_id {a} : I ~ a ⇝ a :=
 by use [2, rfl]
