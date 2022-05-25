4686c022539179d05f05c4de28bae2e2b0cc9255
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-05-25 02:38:20 +0200

    commit

--- src/ski/main.lean
+++ src/ski/main.lean
@@ -22,12 +22,42 @@ inductive Eq : Expr' → Expr' → Prop
 | reduces {a b} : (∃ c, R a c ∧ R b c) → Eq a b
 | ext {a b} : (∀ c, Eq (app' a c) (app' b c)) → Eq a b
 
+-- lemma Eq.reflexive : reflexive Eq :=
+-- by { intro a, left, use a, split; left }
+
+-- lemma Eq.symmetric : symmetric Eq :=
+-- begin
+--   rintro a b h,
+--   induction' h,
+--   { left, tauto },
+--   { right, exact ih },
+-- end
+
 lemma Eq.equivalence : equivalence Eq :=
 begin
-  sorry
+  refine ⟨_, _, _⟩,
+  { intro a, left, use a, split; left },
+  { rintro a b h,
+    induction' h,
+    { left, tauto },
+    { right, exact ih }},
+  {
+    rintro a b x h₁ h₂,
+    cases h₁ with _ _ h₁ _ _ h₁;
+    cases h₂ with _ _ h₂ _ _ h₂,
+    {
+      left,
+      rcases h₁ with ⟨c, hc₁, hc₂⟩,
+      rcases h₂ with ⟨d, hd₁, hd₂⟩,
+      sorry
+    },
+    sorry,
+    sorry,
+    sorry,
+  },
 end
 
--- #exit
+#exit
 
 instance : setoid Expr' := ⟨_, Eq.equivalence⟩
 
