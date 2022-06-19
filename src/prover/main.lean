1ae07411d78f420ec2fd64cf0278216b60f97ced
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-06-19 19:30:40 +0200

    commit

--- src/prover/main.lean
+++ src/prover/main.lean
@@ -8,6 +8,8 @@ namespace test
 
 notation f ` # `:1 a:0 := f a
 
+def id {α : Sort*} (x : α) : α := x
+
 -----
 
 def true : Prop :=
@@ -48,13 +50,9 @@ local infixl ` ≠ `:50 := ne
 
 -----
 
-axiom true_ne_false : true ≠ false
-axiom prop_ind (F : Prop → Prop) {P : Prop} (h₁ : F true) (h₂ : F false) : F P
+axiom prop_rec (F : Prop → Prop) {P : Prop} (h₁ : F true) (h₂ : F false) : F P
 
 -----
-
-lemma id {P : Prop} (h : P) : P :=
-h
 
 lemma trivial : true :=
 λ _, id
@@ -66,9 +64,9 @@ lemma not_not_intro {P : Prop} (h : P) : ¬¬P :=
 λ h₁, h₁ h
 
 lemma not_not_elim {P : Prop} (h : ¬¬P) : P :=
-prop_ind (λ P, ¬¬P → P) (λ _, trivial) (λ h₁, h₁ not_false) h
+prop_rec (λ x, ¬¬x → x) (λ _, trivial) (λ h₁, h₁ not_false) h
 
-lemma lem {P : Prop} : P ∨ ¬P := id
+lemma em {P : Prop} : P ∨ ¬P := id
 
 lemma false_elim {P : Prop} (h : false) : P :=
 h P
@@ -93,6 +91,45 @@ not_not_elim # λ h₁, h # or_intro₁ h₁
 
 lemma and_elim₂ {P Q : Prop} (h : P ∧ Q) : Q :=
 not_not_elim # λ h₁, h # or_intro₂ h₁
+
+lemma iff_intro {P Q : Prop} (h₁ : P → Q) (h₂ : Q → P) : P ↔ Q :=
+and_intro h₁ h₂
+
+lemma iff_elim₁ {P Q : Prop} (h : P ↔ Q) : P → Q :=
+and_elim₁ h
+
+lemma iff_elim₂ {P Q : Prop} (h : P ↔ Q) : Q → P :=
+and_elim₂ h
+
+lemma mp {P Q : Prop} (h : P ↔ Q) : P → Q :=
+iff_elim₁ h
+
+lemma mpr {P Q : Prop} (h : P ↔ Q) : Q → P :=
+iff_elim₂ h
+
+lemma true_ne_false : true ≠ false :=
+λ h, h id trivial
+
+lemma not_not {P : Prop} : ¬¬P ↔ P :=
+iff_intro not_not_elim not_not_intro
+
+lemma iff_rec (F : Prop → Prop) {P Q : Prop} (h₁ : P ↔ Q) (h₂ : F P) : F Q :=
+prop_rec (λ x, (x ↔ Q) → F x → F Q)
+(@prop_rec (λ x, (true ↔ x) → F true → F x) Q (λ _, id) (λ h₃, false_elim # mp h₃ trivial))
+(@prop_rec (λ x, (false ↔ x) → F false → F x) Q (λ h₃, false_elim # mpr h₃ trivial) (λ _, id))
+h₁ h₂
+
+lemma eq_refl {α : Type} {x : α} : x = x :=
+λ _, id
+
+lemma rfl {α : Type} {x : α} : x = x :=
+eq_refl
+
+lemma eq_comm {α : Type} {x y : α} (h : x = y) : y = x :=
+not_not_elim # λ h₁, mt (h (λ z, z = x)) h₁ rfl
+
+lemma eq_iff {α : Type} {x y : α} : x = y ↔ (∀ (P : α → Prop), P x ↔ P y) :=
+iff_intro (λ h P, iff_intro (h P) (eq_comm h P)) (λ h, mp (h (λ z, x = z)) rfl)
 
 -----
 
