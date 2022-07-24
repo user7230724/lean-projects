809e5c91a4e0f72d50b8fdd169a95f6a7816d0a6
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-07-24 21:54:25 +0200

    commit

--- src/m/main.lean
+++ src/m/main.lean
@@ -13,7 +13,7 @@ Inf {n | f n ≠ 0}
 
 ---
 
-@[simp] lemma M_const {a : ℕ} : M (λ _, a) = 0 :=
+@[simp] lemma M_const {a : ℕ} : M (λ (b : ℕ), a) = 0 :=
 begin
   dsimp, by_cases h : a = 0,
   { simp [h] },
@@ -21,6 +21,59 @@ begin
     generalize_proofs h₁, have h₂ : nat.find h₁ ≤ 0 := nat.find_le trivial,
     rw nat.le_zero_iff at h₂, convert h₂ },
 end
+
+@[simp] lemma M_id : M (λ (a : ℕ), a) = 1 :=
+by { dsimp, rw [nat.Inf_def ⟨1, _⟩]; simp [nat.find_eq_iff] }
+
+@[simp] lemma Inf_ge {a : ℕ} : Inf {b | a ≤ b} = a :=
+by { rw nat.Inf_def ⟨a, _⟩; simp [nat.find_eq_iff, le_refl a] }
+
+-- #exit
+
+---
+
+def zero' : ℕ := M (λ (a : ℕ), L a a)
+
+def one' : ℕ := M (λ (a : ℕ), a)
+
+def not' (a : ℕ) : ℕ :=
+M (λ (b : ℕ), L (L a zero') b)
+
+def imp' (a b : ℕ) : ℕ :=
+M (λ (c : ℕ), L (L a b) c)
+
+def or' (a b : ℕ) : ℕ :=
+M (λ (c : ℕ), L (imp' (not' a) b) c)
+
+-- #exit
+
+---
+
+@[simp] lemma zero'_def : zero' = 0 :=
+by simp [zero']
+
+@[simp] lemma one'_def : one' = 1 :=
+by simp [one']
+
+@[simp] lemma not'_def {a : ℕ} : not' a = if a = 0 then 1 else 0 :=
+by simp [not']
+
+@[simp] lemma zero'_imp' {a : ℕ} : imp' zero' a = 1 :=
+by simp [imp']
+
+@[simp] lemma imp'_zero' {a : ℕ} : imp' a zero' = not' a :=
+by simp [imp']
+
+@[simp] lemma one_imp' {a : ℕ} : imp' 1 a = if a = 0 then 0 else 1 :=
+begin
+  simp only [imp', M, L, ne.def, ite_eq_right_iff, nat.one_ne_zero,
+  not_forall, not_false_iff, exists_prop, and_true, Inf_ge], rw ←ite_not, simp,
+end
+
+@[simp] lemma zero'_or' {a : ℕ} : or' zero' a = if a = 0 then 0 else 1 :=
+by simp [or']
+
+-- #exit
 
 ---
 
